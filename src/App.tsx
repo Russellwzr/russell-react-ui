c2fb777f7e3f9ae110382c31ba58f3f81aba39a0
Russellwzr <russellwzrr@gmail.com>
2023-05-31 17:02:25 +0800

    feat: add Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import React from 'react'
+import { Button } from './components/Button'
 
 function App() {
   return (
     <div>
       <header>
-        <h1>Hello World</h1>
-        <h2>Hello World</h2>
-        <h3>Hello World</h3>
-        <code>const a = 'b'</code>
+        <Button className="custom">Hello</Button>
+        <Button disabled>Hello</Button>
+        <Button btnType="primary" size="lg">
+          Hello
+        </Button>
+        <Button btnType="danger" size="sm">
+          Hello
+        </Button>
+        <Button btnType="link" size="lg">
+          Hello
+        </Button>
+        <Button btnType="default" size="lg" disabled>
+          Hello
+        </Button>
       </header>
     </div>
   )
